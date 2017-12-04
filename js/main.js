9b2665989b48b06e3f437174b0a6950b8f81a346
Ross Correia <rmcorre@gmail.com>
2017-12-04 12:20:12 -0100

    Added console logs
    Trying different ways of debugging

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
 (function () {
     var on = false;
     var strict = false; //check if strict is a reserved word
-    var player = [];
-    var pattern = [];
+    var playerArr = [];
+    var patternArr = [];
     var patternLength = 1;
-    var patternIndex = 0; //consider renaming this to arrayIndex  
-    var pad = null;
-    //var i = null; //iterator, should be initialized to a number
-    //var j = null; //iterator, should be initialized to a number
+    var patternIndex = 0;  
     var num = 0; //consider renaming this to something more specific
+    var pad = null;
     var element = null;
-    var padClicked = 0; //Holds the value of the data-id attribute of the pad clicked: 
-                        //Green = 1, Red = 2, Blue = 3,  yellow = 4
+    var padClicked = 0; //Holds the value of the data-id attribute of the pad clicked: Green = 1, Red = 2, Blue = 3,  yellow = 4
     var tone = null;
+    var padGroupOnStatus = false;
+
     var checkForClickId = null; //check if setTimeout returns an object type or a number type
     var genPatternId = null;
     var flashId = null;
     var displayId1 = null;
     var displayId2 = null;
-    var padGroupOnId = null;
-    var padGroupOnStatus = false;
+    var padGroupOnId = null;    
 
     //cache dom
     var simon = $("#simon");
@@ -37,29 +35,44 @@
 
     function onOff() {
         if (!on) {
+            //initialize "on" state
             on = true;
             buttonOff.css("visibility", "hidden");
             buttonOn.css("visibility", "visible");
             displayText.text("--");
-            strictButton.on("click", strictPlay);
             startButton.on("click", start);
+            strictButton.on("click", strictPlay);
 
+            console.groupCollapsed("%cfunction on/off %con = %c" + on, "color: blue;", "color: black;", "color: chocolate;");
+                console.log("%cdisplayText.text('--');", "color: gray");
+                console.log("%cstartButton.on('click', start);", "color: gray");
+                console.log("%cstrictButton.on('click', strictPlay);", "color: gray");
+                console.log("padGroupOnStatus = %c" + padGroupOnStatus, "color: chocolate;");
+            console.groupEnd();
         }
         else {
+            //initialize "off" state
             on = false;
+            buttonOff.css("visibility", "visible");
+            buttonOn.css("visibility", "hidden");
+            displayText.text("");
+            startButton.off();
+            strictButton.off();
+            padGroupOff();
+
+            //reset variables
             strict = false;
-            player = [];
-            pattern = [];
+            playerArr.length = 0;
+            patternArr.length = 0;
             patternLength = 1;
             patternIndex = 0;
-            pad = null;
-            //i = null;
-            //j = null;
             num = 0;
+            pad = null;
             element = null;
             padClicked = 0;
             tone = null;
 
+            //reset timeouts
             clearTimeout(checkForClickId);
             checkForClickId = null;
             clearTimeout(genPatternId);
@@ -73,15 +86,24 @@
             clearTimeout(padGroupOnId);
             padGroupOnId = null;
 
-            buttonOff.css("visibility", "visible");
-            buttonOn.css("visibility", "hidden");
-
-            displayText.text("");
-            startButton.off();
-            strictButton.off();
-            padGroupOff();
-            return;
+            console.groupCollapsed("%cfunction on/off %con = %c" + on, "color: blue;", "color: black;", "color: chocolate;");
+                console.log("%cdisplayText.text('');", "color: gray");
+                console.log("%cstartButton.off();", "color: gray");
+                console.log("%cstrictButton.off();", "color: gray");
+                console.log("strict = %c" + strict, "color: chocolate;");
+                console.log("playerArr.length = %c" + playerArr.length, "color: chocolate;");
+                console.log("patternArr.length = %c" + patternArr.length, "color: chocolate;");
+                console.log("patternLength = %c" + patternLength, "color: chocolate;");
+                console.log("patternIndex = %c" + patternIndex, "color: chocolate;");
+                console.log("num = %c" + num, "color: chocolate;");
+                console.log("pad = %c" + pad, "color: chocolate;");
+                console.log("element = %c" + element, "color: chocolate;");
+                console.log("padClicked = %c" + padClicked, "color: chocolate;");
+                console.log("tone = %c" + tone, "color: chocolate;");
+                console.log("padGroupOnStatus = %c" + padGroupOnStatus, "color: chocolate;");
+            console.groupEnd();
         }
+        return;
     }
 
 
@@ -96,20 +118,41 @@
         displayId1 = null;
         clearTimeout(displayId2);
         displayId2 = null;
-
-
-        padClicked = 0;
-        player.length = 0;
-        pattern.length = 0;
-        getPattern();
-        console.log(pattern);
-
+        
+        strict = false;
+        playerArr.length = 0;
+        patternArr.length = 0;
         patternLength = 1;
+        patternIndex = 0;
+        num = 0;
+        pad = null;
+        element = null;
+        padClicked = 0;
+        tone = null;
+        padGroupOnStatus = false;
+
+        getPattern();
         display("--", 2, 0);
         display(patternLength, 0, 2000);
         genPattern(patternIndex, patternLength, 3500);
         padGroupOn(4500);
         checkForClick(4500 + 5000);
+
+        console.groupCollapsed("%cfunction start", "color: blue;");
+            console.log("strict = %c" + strict, "color: chocolate;");
+            console.log("playerArr.length = %c" + playerArr.length, "color: chocolate;");
+            console.log("patternArr.length = %c" + patternArr.length, "color: chocolate;");
+            console.log("patternLength = %c" + patternLength, "color: chocolate;");
+            console.log("patternIndex = %c" + patternIndex, "color: chocolate;");
+            console.log("num = %c" + num, "color: chocolate;");
+            console.log("pad = %c" + pad, "color: chocolate;");
+            console.log("element = %c" + element, "color: chocolate;");
+            console.log("padClicked = %c" + padClicked, "color: chocolate;");
+            console.log("tone = %c" + tone, "color: chocolate;");
+            console.log("padGroupOnStatus = %c" + padGroupOnStatus, "color: chocolate;");
+        console.groupEnd();
+
+        return;
     }
 
 
@@ -120,7 +163,22 @@
         else {
             strict = false;
         }
-        console.log(strict);
+        
+        console.groupCollapsed("%cfunction strictPlay", "color: blue;");
+            console.log("strict = %c" + strict, "color: chocolate;");
+            console.log("playerArr.length = %c" + playerArr.length, "color: chocolate;");
+            console.log("patternArr.length = %c" + patternArr.length, "color: chocolate;");
+            console.log("patternLength = %c" + patternLength, "color: chocolate;");
+            console.log("patternIndex = %c" + patternIndex, "color: chocolate;");
+            console.log("num = %c" + num, "color: chocolate;");
+            console.log("pad = %c" + pad, "color: chocolate;");
+            console.log("element = %c" + element, "color: chocolate;");
+            console.log("padClicked = %c" + padClicked, "color: chocolate;");
+            console.log("tone = %c" + tone, "color: chocolate;");
+            console.log("padGroupOnStatus = %c" + padGroupOnStatus, "color: chocolate;");
+        console.groupEnd();
+
+        return;
     }
 
 
@@ -147,6 +205,20 @@
                 displayText.text(content);
             }, delay);
         }
+
+        console.groupCollapsed("%cfunction display", "color: blue;");
+            console.log("strict = %c" + strict, "color: chocolate;");
+            console.log("playerArr.length = %c" + playerArr.length, "color: chocolate;");
+            console.log("patternArr.length = %c" + patternArr.length, "color: chocolate;");
+            console.log("patternLength = %c" + patternLength, "color: chocolate;");
+            console.log("patternIndex = %c" + patternIndex, "color: chocolate;");
+            console.log("num = %c" + num, "color: chocolate;");
+            console.log("pad = %c" + pad, "color: chocolate;");
+            console.log("element = %c" + element, "color: chocolate;");
+            console.log("padClicked = %c" + padClicked, "color: chocolate;");
+            console.log("tone = %c" + tone, "color: chocolate;");
+            console.log("padGroupOnStatus = %c" + padGroupOnStatus, "color: chocolate;");
+        console.groupEnd();
     }
 
 
@@ -156,8 +228,8 @@
 
         if (idx < cnt) {
             genPatternId = setTimeout(function () {
-                pad = simon.find("[data-id = " + pattern[idx] + "]");
-                tone = simon.find("#audio" + pattern[idx]);
+                pad = simon.find("[data-id = " + patternArr[idx] + "]");
+                tone = simon.find("#audio" + patternArr[idx]);
                 flash(pad, 500);
                 playSound(tone);
                 idx += 1;
@@ -174,7 +246,7 @@
         if (padClicked === 0) {
             checkForClickId = setTimeout(function () {
                 padGroup.off("click");
-                player.length = 0;
+                playerArr.length = 0;
                 num = 0;
                 display("!!", 2, 0);
                 display(patternLength, 0, 2000);
@@ -183,6 +255,20 @@
                 checkForClick(3500 + (1000 * patternLength) + 5000);
             }, delay);
         }
+
+        console.groupCollapsed("%cfunction checkForClick", "color: blue;");
+            console.log("strict = %c" + strict, "color: chocolate;");
+            console.log("playerArr.length = %c" + playerArr.length, "color: chocolate;");
+            console.log("patternArr.length = %c" + patternArr.length, "color: chocolate;");
+            console.log("patternLength = %c" + patternLength, "color: chocolate;");
+            console.log("patternIndex = %c" + patternIndex, "color: chocolate;");
+            console.log("num = %c" + num, "color: chocolate;");
+            console.log("pad = %c" + pad, "color: chocolate;");
+            console.log("element = %c" + element, "color: chocolate;");
+            console.log("padClicked = %c" + padClicked, "color: chocolate;");
+            console.log("tone = %c" + tone, "color: chocolate;");
+            console.log("padGroupOnStatus = %c" + padGroupOnStatus, "color: chocolate;");
+        console.groupEnd();
     }
 
 
@@ -195,7 +281,7 @@
 
     function getPattern() {
         for (let j = 20 - 1; j >= 0; j -= 1) {
-            pattern.push(getRandom(1, 4));
+            patternArr.push(getRandom(1, 4));
         }
     }
 
@@ -203,7 +289,7 @@
         if (padGroupOnStatus) {
             padGroup.off("click");
             padGroupOnStatus = false;
-            console.log("padGroup on = " + padGroupOnStatus);
+            //console.log("padGroupOnStatus = " + padGroupOnStatus);
         }        
     }
 
@@ -214,7 +300,7 @@
                 padGroup.on("click", padClick);
             }, delay);
             padGroupOnStatus = true;
-            console.log("padGroup on = " + padGroupOnStatus);
+            //console.log("padGroupOnStatus = " + padGroupOnStatus);
         }
     }
 
@@ -244,35 +330,35 @@
         element = $(this); //get the element clicked (#padGre, #padRed, #padYel, #padBlu)
         padClicked = parseInt(element.data("id"), 10); //get the value of the data-id attribute (1, 2, 3, 4)
         tone = element.find("#audio" + padClicked);
-        //var patternLength = pattern.slice(0, patternLength).length;
+        //var patternLength = patternArr.slice(0, patternLength).length;
 
         flash(element, 500);
         playSound(tone);
 
-        if (padClicked === pattern[num] && num < patternLength) {
-            // if id's match, push element clicked into player array
+        if (padClicked === patternArr[num] && num < patternLength) {
+            // if id's match, push element clicked into playerArr array
             clearTimeout(checkForClickId);
             checkForClickId = null;
             
-            console.log("%c Get pattern at current patternLength length (patternLength starts at 1 and increment by 1 on each iteration)", "color: blue;");
+            console.log("%c Get pattern at current patternLength (patternLength starts at 1 and increment by 1 on each iteration)", "color: blue;");
             console.log("%c patternLength = " + patternLength, "color: chocolate;");
-            console.log("pattern = " + "[" + pattern + "]");
+            console.log("patternArr = " + "[" + patternArr + "]");
             //console.log("pattern size =" + "%c " + patternLength + " (patternLength)", "color: chocolate;");
-            console.log("pattern.slice(0," + "%c patternLength)     " + "// " + patternLength, "color: chocolate;");
-            console.log("pattern = " + "[" + pattern.slice(0, patternLength) + "]");
+            console.log("patternArr.slice(0," + "%c patternLength)     " + "// " + patternLength, "color: chocolate;");
+            console.log("pattern = " + "[" + patternArr.slice(0, patternLength) + "]");
 
             console.log("\n");
 
-            console.log("%c Check if padClicked === value of pattern starting at index 0 (num)", "color: blue;");
+            console.log("%c Check if value of padClicked === value of patternArr[num]", "color: blue;");
             console.log("%c padClicked = " + padClicked, "color: chocolate");
             //console.log("Id of pad clicked = padClicked     " + "//" + padClicked);
             console.log("%c num = " + num, "color: cadetblue");            
             //console.log("Value of pattern[num] = " + pattern[num]);            
-            console.log("if padClicked === pattern[" + num + "]     " + "%c // " + padClicked + " === " + pattern[num], "color: chocolate;");
-            console.log("player.push(padClicked)");                       
+            console.log("if padClicked === patternArr[" + num + "]     " + "%c // " + padClicked + " === " + patternArr[num], "color: chocolate;");
+            console.log("playerArr.push(padClicked)");                       
 
-            player.push(padClicked);
-            console.log("player = " + "[" + player + "]");
+            playerArr.push(padClicked);
+            console.log("playerArr = " + "[" + playerArr + "]");
             num += 1;
             console.log("num += 1");
             console.log("\n");
@@ -286,7 +372,7 @@
                 clearTimeout(checkForClickId);
                 checkForClickId = null;
 
-                console.error("Error: padClicked does not match pattern[num]");
+                console.error("Error: padClicked does not match patternArr[num]");
                 console.log("strict = " + strict);
                 console.log("%c Get pattern at current patternLength length (patternLength starts at 1 and increment by 1 on each iteration)", "color: blue;");
                 console.log("%c patternLength = " + patternLength, "color: chocolate;");
@@ -303,9 +389,9 @@
                 console.log("%c num = " + num, "color: cadetblue");            
                 //console.log("Value of pattern[num] = " + pattern[num]);            
                 console.log("if padClicked === pattern[" + num + "]     " + "%c // " + padClicked + " === " + pattern[num], "color: chocolate;");
-                console.log("player.push(padClicked)");
+                console.log("playerArr.push(padClicked)");
                 num = 0;
-                console.log("player[" + player + "] === " + "pattern[" + pattern.slice(0, patternLength) + "]");
+                console.log("playerArr[" + playerArr + "] === " + "pattern[" + pattern.slice(0, patternLength) + "]");
                 console.log("start();");
                 start();
                 return;
@@ -320,25 +406,25 @@
         }
 
 
-        // if pattern is complete - player array matches pattern array
-        if (player.equals(pattern.slice(0, patternLength))) {
+        // if pattern is complete - playerArr matches patternArr
+        if (playerArr.equals(patternArr.slice(0, patternLength))) {
             // if arrays at current patternLength match
-            console.log("%c Check if player array === pattern array", "color: blue;");
-            console.log("player[" + player + "] === " + "pattern[" + pattern.slice(0, patternLength) + "]");
+            console.log("%c Check if playerArr === patternArr", "color: blue;");
+            console.log("playerArr[" + playerArr + "] === " + "patternArr[" + patternArr.slice(0, patternLength) + "]");
             console.log("\n");
             console.log("\n");
             clearTimeout(checkForClickId);
             checkForClickId = null;
 
             // check if win
-            if (player.length === 5) {
+            if (playerArr.length === 5) {
                 // check for win
                 display("win", 0, 0);
             }
             else {
                 // else reset and increase patternLength by 1 - 
                 padClicked = 0;
-                player.length = 0;
+                playerArr.length = 0;
                 num = 0;
                 patternLength += 1;
                 display(patternLength, 0, 0);
